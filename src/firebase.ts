f93c3342902490a70c53414d4c35c99ab866911a
harumaki2000 <harumaki2000.net@gmail.com>
2025-02-19 15:08:11 +0900

    .envを削除

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/
 import { getFirestore, collection, addDoc, query, where, getDocs } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'your_apiKey',
+  authDomain: 'your_authDomain',
+  projectId: 'your_projectId',
+  storageBucket: 'your_storageBucket',
+  messagingSenderId: 'your_messagingSenderId',
+  appId: 'your_appId',
+  measurementId: 'your_measurementId',
 };
 
 const app = initializeApp(firebaseConfig);
